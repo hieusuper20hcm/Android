81992f8670caa8e80f315de07984ea029e7da332
hieusuper20hcm <64774119+hieusuper20hcm@users.noreply.github.com>
2020-10-10 22:16:37 +0700

    Update auth.controller.js

--- Api/controller/auth.controller.js
+++ Api/controller/auth.controller.js
@@ -6,10 +6,10 @@ module.exports.login=async function(req,res,next){
     const password=req.body.password;
     const user= await User.findOne({email:email});
      if(!user){
-         return res.json({msg: 'Email không tồn tại'})
+         return res.json({msg: "Email không tồn tại"})
      }
      if(md5(password)!=user.password){
-        return res.json({msg: 'Sai mật khẩu'})
+        return res.json({msg: "Sai mật khẩu"})
      }
-     res.json({msg: 'Đăng nhập thành công'})
+     res.json({msg: "Đăng nhập thành công"})
 }
