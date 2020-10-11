6a205e22932dde0ce02cf11f9649847cbec78f6a
Minh Hieu <minhhieu0112000@gmail.com>
2020-10-11 19:04:51 +0700

    Sua

--- Api/controller/cart.controller.js
+++ Api/controller/cart.controller.js
@@ -7,21 +7,30 @@ module.exports.index=async (req,res)=>{
 
 module.exports.addCart = async (req, res) => {
     const cartItem=await Cart.find({userID:req.body.userID});
-    const query=[{'cart.productID':req.body.cart.productID},
-                {'cart.size':req.body.cart.size},{'cart.color':req.body.cart.color},
+    const query=[{productID:req.body.productID},
+                {size:req.body.size},{color:req.body.color},
                 {userID:req.body.userID}];
     const cart=await Cart.findOne({$and:query});
+    let object={
+        productID:req.body.productID,
+        name:req.body.name,
+        price:req.body.price,
+        img:req.body.img,
+        count:req.body.count,
+        size:req.body.size,
+        color:req.body.color
+    }
     if(cartItem.length>0)
     {
        if(cart){
-           let count=cart.cart.count+req.body.cart.count
-            await Cart.updateMany({'cart.productID':req.body.cart.productID},{'cart.count':count})
+           let count=cart.count+req.body.count
+            await Cart.updateMany({productID:req.body.productID},{count:count})
             res.json("Đặt hàng thành công");
        }
        else{
             await Cart.create({
                 userID:  req.body.userID,
-                cart: req.body.cart
+                object
             })
             res.json("Đặt hàng thành công");
        }
@@ -29,7 +38,7 @@ module.exports.addCart = async (req, res) => {
     else{
         await Cart.create({
             userID:  req.body.userID,
-            cart: req.body.cart
+            object
         })
         res.json("Đặt hàng thành công");
     }
--- Api/model/cart.model.js
+++ Api/model/cart.model.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose')
 const cartSchema = new mongoose.Schema({
                 userID: String,
-                cart:{
-                    productID:String,
-                    name: String,
-                    price: Number,
-                    img: String,
-                    count:Number,
-                    size: String,
-                    color: String
-                }           
+                productID:String,
+                name: String,
+                price: Number,
+                img: String,
+                count:Number,
+                size: String,
+                color: String           
 });
 const Cart = mongoose.model('Cart', cartSchema,'carts');
 module.exports=Cart;
